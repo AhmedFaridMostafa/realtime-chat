4b2cd025629307eeaccdf1f471cb23b3ac53e1b7
Ahmed Fared <ahmed.super997@gmail.com>
2025-08-13 04:13:03 +0300

    feat: add user authentication with google
     - Integrate Next-Auth for OAuth authentication
     - Add google OAuth provider configuration
     - Configure environment variables for auth
     - Add protected routes

---
+++ src/lib/auth.config.ts
@@ -0,0 +1,62 @@
+import { UpstashRedisAdapter } from "@auth/upstash-redis-adapter";
+import { NextAuthConfig } from "next-auth";
+import { db } from "./db";
+import Google from "next-auth/providers/google";
+
+// function getGoogleCredentials() {
+//   const clientId = process.env.GOOGLE_CLIENT_ID;
+//   const clientSecret = process.env.GOOGLE_CLIENT_SECRET;
+
+//   if (!clientId || clientId.length === 0) {
+//     throw new Error("Missing GOOGLE_CLIENT_ID");
+//   }
+
+//   if (!clientSecret || clientSecret.length === 0) {
+//     throw new Error("Missing GOOGLE_CLIENT_SECRET");
+//   }
+
+//   return { clientId, clientSecret };
+// }
+
+export const authConfig: NextAuthConfig = {
+  adapter: UpstashRedisAdapter(db),
+  session: {
+    strategy: "jwt",
+  },
+  pages: {
+    signIn: "/login",
+  },
+  providers: [Google],
+  callbacks: {
+    async jwt({ token, user }) {
+      const dbUser = (await db.get(`user:${token.id}`)) as User | null;
+      if (!dbUser) {
+        if (user) {
+          token.id = user.id! ?? token.sub!;
+        }
+        return token;
+      }
+      return {
+        id: dbUser.id,
+        name: dbUser.name,
+        email: dbUser.email,
+        picture: dbUser.image,
+      };
+    },
+    async session({ session, token }) {
+      if (token) {
+        session.user.id = token.id;
+        session.user.name = token.name;
+        session.user.email = token.email!;
+        session.user.image = token.picture;
+      }
+      return session;
+    },
+    redirect() {
+      return "/dashboard";
+    },
+    async authorized({ auth }) {
+      return !!auth?.user;
+    },
+  },
+};
---
+++ src/lib/validations/validations.ts
@@ -0,0 +1,14 @@
+import { z } from "zod";
+
+export const messageValidator = z.object({
+  id: z.string(),
+  senderId: z.string(),
+  text: z.string(),
+  timestamp: z.number(),
+});
+
+export const messageArrayValidator = z.array(messageValidator);
+
+export const addFriendValidator = z.object({
+  email: z.email(),
+});
